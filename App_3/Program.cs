87586705ed2b0b9e92494cd8d2d6c862de2c47d2
Nikita Belov <lolvvto@yandex.ru>
2022-10-24 19:18:37 +0700

    Add App 3

---
+++ App_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void PrintCubesToConsole(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine(i * i * i);
+    }
+}
+
+PrintCubesToConsole(3);
